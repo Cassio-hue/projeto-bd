f1fc2f27c79049261f42147d148b4cf681a2f001
Cassio-hue <cassiocvtb2003@gmail.com>
2023-07-04 10:34:15 -0300

    feat: página para fazer login

--- frontend/src/app/components/Input.tsx
+++ frontend/src/app/components/Input.tsx
@@ -1,12 +1,27 @@
 'use client'
 
+import { Controller, useFormContext } from 'react-hook-form'
 import { TextField } from '@mui/material'
 import { TextFieldProps } from '@mui/material'
 
 type InputProps = TextFieldProps & {
   label?: string
+  name: string
 }
 
-export const Input = ({ label }: InputProps) => {
-  return <TextField label={label} sx={{borderColor: '#FFF'}} />
+export const Input = ({ label, name }: InputProps) => {
+  const methods = useFormContext()
+
+  return (
+    <Controller
+      control={methods.control}
+      name={name}
+      render={({ field }) => (
+        <TextField
+          label={label}
+          {...field}
+        />
+      )}
+    />
+  )
 }
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,9 +1,43 @@
+'use client'
+
+import clsx from 'clsx'
 import { Input } from '../components/Input'
+import { useForm, FormProvider, SubmitHandler } from 'react-hook-form'
+import { Button } from '../components/Button'
+
+interface DataType {
+  email: string
+  senha: string
+}
 
 export default function Login() {
+
+  const userFormDefaultValues: DataType = {
+    email: '',
+    senha: ''
+  };
+
+  const methods = useForm<DataType>({
+    defaultValues: userFormDefaultValues,
+  });
+
+
+  const onSubmit: SubmitHandler<DataType> = (data) => console.log(data)
   return (
-    <>
-      <Input />
-    </>
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} className={clsx('flex flex-col gap-8')}>
+        <Input
+          name="email"
+          type="email"
+          label={'E-mail'}
+        />
+        <Input
+          name="password"
+          type="password"
+          label={'Senha'}
+        />
+        <Button type="submit">Entrar</Button>
+      </form>
+    </FormProvider>
   )
 }
