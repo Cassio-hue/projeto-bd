e31cb4e3a7cb0ff359f7054f58e69989528ed5b4
Cassio-hue <cassiocvtb2003@gmail.com>
2023-07-04 01:13:00 -0300

    feat: primeiro commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx'
+import './globals.css'
+
+export const metadata = {
+  title: 'SIGAA 2',
+  description: 'Trabalho da disciplina Banco de Dados da Universidade de Brasília 2023.1',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={clsx("flex h-screen justify-center items-center m-auto")}>{children}</body>
+    </html>
+  )
+}
