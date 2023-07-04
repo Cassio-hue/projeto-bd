f003bce04938d211463458885df158db04991c1b
Cassio-hue <cassiocvtb2003@gmail.com>
2023-07-04 02:16:39 -0300

    feat: component card para mostrar as disciplinas

---
+++ frontend/src/app/components/Card.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx'
+
+export interface CardProps {
+    discipline: string;
+    department: string;
+    period: string;
+    teacher: string;
+  }
+  
+interface Props {
+  values: CardProps[];
+}
+
+export const Card = ({ values }: Props) => {
+  return (
+    <>
+      {values.map((item, idx) => (
+        <div
+          key={idx}
+          className={clsx(
+            'flex flex-col bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl w-1/2 p-1 m-3'
+          )}
+        >
+          <div className={clsx('flex flex-col rounded-2xl p-5 bg-black')}>
+            <h1 className={clsx('text-2xl mb-4')}>{item.discipline}</h1>
+            <span className={clsx('font-bold')}>
+              Departamento:
+              <span className={clsx('font-normal ml-2')}>{item.department}</span>
+            </span>
+            <span className={clsx('font-bold')}>
+              Periodo:
+              <span className={clsx('font-normal ml-2')}>{item.period}</span>
+            </span>
+            <span className={clsx('font-bold')}>
+              Professor:
+              <span className={clsx('font-normal ml-2')}>{item.teacher}</span>
+            </span>
+          </div>
+        </div>
+      ))}
+    </>
+  )
+}
