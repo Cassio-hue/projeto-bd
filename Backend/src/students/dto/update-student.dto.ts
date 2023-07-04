9f3a893bc34550c1abd67a6da3a192379fa3f379
Cassio-hue <cassiocvtb2003@gmail.com>
2023-07-03 22:17:16 -0300

    refactor: adição de validadores no dto para atualizar estudante

--- Backend/src/students/dto/update-student.dto.ts
+++ Backend/src/students/dto/update-student.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class UpdateStudentDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
-  photo: string;
 }
