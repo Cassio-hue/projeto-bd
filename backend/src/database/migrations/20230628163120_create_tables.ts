81ce2a5f6c02f5f6b6741aaf4317a2f5b3eb8062
Cassio-hue <cassiocvtb2003@gmail.com>
2023-07-05 18:38:42 -0300

    refactor: modificando algumas tabelas

--- backend/src/database/migrations/20230628163120_create_tables.ts
+++ backend/src/database/migrations/20230628163120_create_tables.ts
@@ -2,101 +2,101 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.raw(`
-  CREATE TABLE IF NOT EXISTS Student (
-    id SERIAL PRIMARY KEY,
-    studentID CHAR(9) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+  CREATE TABLE IF NOT EXISTS student (
+    id SERIAL PRIMARY KEY UNIQUE,
+    student_id CHAR(9) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
-    UNIQUE (id),
-    UNIQUE (email),
-    UNIQUE (studentID)
+    is_admin BOOLEAN NOT NULL
   );
 
-  CREATE TABLE IF NOT EXISTS Department (
-    id SERIAL PRIMARY KEY,
-    code INT NOT NULL,
-    departmentName VARCHAR(200) NOT NULL,
-    UNIQUE (id),
-    UNIQUE (code)
+  CREATE TABLE IF NOT EXISTS department (
+    id SERIAL PRIMARY KEY UNIQUE,
+    code INT NOT NULL UNIQUE,
+    departament_name VARCHAR(100) NOT NULL
   );
   
-  CREATE TABLE IF NOT EXISTS Teacher (
-    id SERIAL PRIMARY KEY,
-    teacherID CHAR(9) NOT NULL,
+  CREATE TABLE IF NOT EXISTS teacher (
+    id SERIAL PRIMARY KEY UNIQUE,
+    teacher_id CHAR(9) NOT NULL UNIQUE,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(45) NOT NULL,
-    UNIQUE (id),
-    UNIQUE (email),
-    UNIQUE (teacherID),
-    department_id INT,
-    CONSTRAINT department
-      FOREIGN KEY (department_id)
-      REFERENCES Department (id)
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    departament_id INT,
+    CONSTRAINT fk_departament_id
+      FOREIGN KEY (departament_id)
+      REFERENCES department (id)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION
   );
   
-  CREATE TABLE IF NOT EXISTS Discipline (
-    id SERIAL PRIMARY KEY,
-    code VARCHAR(8) NOT NULL,
-    disciplineName VARCHAR(45) NOT NULL,
-    department_id INT,
-    CONSTRAINT departmentCode
-      FOREIGN KEY (department_id)
-      REFERENCES Department (id)
+  CREATE TABLE IF NOT EXISTS discipline (
+    id SERIAL PRIMARY KEY UNIQUE,
+    code VARCHAR(8) NOT NULL UNIQUE,
+    discipline_name VARCHAR(45) NOT NULL,
+    departament_id INT,
+    CONSTRAINT fk_departament_id
+      FOREIGN KEY (departament_id)
+      REFERENCES department (id)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+    teacher_id INT,
+    CONSTRAINT fk_teacher_id
+      FOREIGN KEY (teacher_id)
+      REFERENCES teacher (id)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION
-  );
+  );  
   
-  CREATE TABLE IF NOT EXISTS Rating (
+  CREATE TABLE IF NOT EXISTS rating (
     id SERIAL PRIMARY KEY,
     score INT NOT NULL,
+    comment VARCHAR(250),
+    reports INT,
     student_id INT,
-    CONSTRAINT student
+    CONSTRAINT fk_student_id
       FOREIGN KEY (student_id)
-      REFERENCES Student (id)
+      REFERENCES student (id)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION
   );
   
-  CREATE TABLE IF NOT EXISTS Class (
-    id SERIAL PRIMARY KEY,
-    className VARCHAR(100),
-    schedule VARCHAR(45) NOT NULL,
+  CREATE TABLE IF NOT EXISTS class (
+    id SERIAL PRIMARY KEY UNIQUE,
+    turma INT NOT NULL,
     period VARCHAR(45) NOT NULL,
-    classCode VARCHAR(45) NOT NULL,
+    schedule VARCHAR(45) NOT NULL,
     local VARCHAR(45) NOT NULL,
     discipline_id INT,
-    rating_id INT,
-    teacher_id INT,
-    CONSTRAINT discipline
+    CONSTRAINT fk_discipline_id
       FOREIGN KEY (discipline_id)
-      REFERENCES Discipline (id)
+      REFERENCES discipline (id)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION,
-    CONSTRAINT rating
+    rating_id INT,
+    CONSTRAINT fk_rating_id
       FOREIGN KEY (rating_id)
-      REFERENCES Rating (id)
+      REFERENCES rating (id)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION,
-    CONSTRAINT teacher
+    teacher_id INT,
+    CONSTRAINT fk_teacher_id
       FOREIGN KEY (teacher_id)
-      REFERENCES Teacher (id)
+      REFERENCES teacher (id)
       ON DELETE CASCADE
       ON UPDATE NO ACTION
-    );
+  );
   `);
 }
 
 export async function down(knex: Knex): Promise<void> {
   return knex.raw(`
-  DROP TABLE IF EXISTS Student CASCADE;
-  DROP TABLE IF EXISTS Department CASCADE;
-  DROP TABLE IF EXISTS Teacher CASCADE;
-  DROP TABLE IF EXISTS Discipline CASCADE;
-  DROP TABLE IF EXISTS Rating CASCADE;
-  DROP TABLE IF EXISTS Class CASCADE;
+  DROP TABLE IF EXISTS student CASCADE;
+  DROP TABLE IF EXISTS department CASCADE;
+  DROP TABLE IF EXISTS teacher CASCADE;
+  DROP TABLE IF EXISTS discipline CASCADE;
+  DROP TABLE IF EXISTS rating CASCADE;
+  DROP TABLE IF EXISTS class CASCADE;
   `);
 }
