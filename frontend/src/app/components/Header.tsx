ed8dd8925c3dfd1ec629edf93d2e75f2dacc8bea
Cassio-hue <cassiocvtb2003@gmail.com>
2023-07-07 22:11:03 -0300

    feat: botão para sair da aplicação

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,19 +1,26 @@
+'use client'
+
 import clsx from 'clsx'
 import { NavOption } from './NavOption'
+import { logout } from '../api/api'
 
 export default function Header() {
   return (
     <header className="flex items-center h-12 w-full bg-slate-700 text-white">
       <nav className={clsx('flex w-full justify-center')}>
-        <NavOption to="/home">
-          Home
-        </NavOption>
-        <NavOption to="/turmas">
-          Turmas
-        </NavOption>
-        <NavOption to="/perfil">
-          Perfil
-        </NavOption>
+        <div>
+          <NavOption to="/home">Home</NavOption>
+          <NavOption to="/turmas">Turmas</NavOption>
+          <NavOption to="/perfil">Perfil</NavOption>
+          <span
+            className="ml-4 text-red-600 cursor-pointer"
+            onClick={() => {
+              logout()
+            }}
+          >
+            Sair
+          </span>
+        </div>
       </nav>
     </header>
   )
