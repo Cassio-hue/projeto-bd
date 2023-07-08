b70cfb24aa6752e049aab5ef6c1684fbdd5b2515
Cassio-hue <cassiocvtb2003@gmail.com>
2023-07-07 22:03:11 -0300

    refactor: retirando a unicidade do nome do estudante

--- backend/src/database/migrations/20230628163120_create_tables.ts
+++ backend/src/database/migrations/20230628163120_create_tables.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
   CREATE TABLE IF NOT EXISTS student (
     id SERIAL PRIMARY KEY UNIQUE,
     student_id CHAR(9) NOT NULL UNIQUE,
-    name VARCHAR(100) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     is_admin BOOLEAN NOT NULL
