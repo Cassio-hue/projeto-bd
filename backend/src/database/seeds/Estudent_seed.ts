35015a5fe2895d2c38762e7b17d81a65d9c67363
Cassio-hue <cassiocvtb2003@gmail.com>
2023-07-09 12:10:19 -0300

    refactor: somente uma imagem para seed do estudante

--- backend/src/database/seeds/Estudent_seed.ts
+++ backend/src/database/seeds/Estudent_seed.ts
@@ -1,25 +1,13 @@
 import { Knex } from 'knex';
 import * as fs from 'fs';
 
-const image1 = fs.readFileSync(
-  __dirname + '/images/profilePicture1.jpg',
-  'base64',
-);
-const image2 = fs.readFileSync(
-  __dirname + '/images/profilePicture2.jpg',
-  'base64',
-);
-const image3 = fs.readFileSync(
-  __dirname + '/images/profilePicture3.jpg',
+const image = fs.readFileSync(
+  __dirname + '/images/profilePicture.png',
   'base64',
 );
 
-let buffer = Buffer.from(image1, 'base64');
-const profilePicture1 = buffer.toString('hex');
-buffer = Buffer.from(image2, 'base64');
-const profilePicture2 = buffer.toString('hex');
-buffer = Buffer.from(image3, 'base64');
-const profilePicture3 = buffer.toString('hex');
+const buffer = Buffer.from(image, 'base64');
+const profilePicture = buffer.toString('hex');
 
 export async function seed(knex: Knex): Promise<void> {
   //   Deletes ALL existing entries
@@ -31,8 +19,8 @@ export async function seed(knex: Knex): Promise<void> {
 
   INSERT INTO student (student_id, name, email, password, is_admin, picture)
   VALUES
-  ('824584468', 'José Coelho Carvalho', 'joaobunny@gmail.com', '123', false, decode('${profilePicture1}', 'hex')),
-  ('519692670', 'Leonardo Kimura Marques', 'leokim@hotmail.com', '123', false, decode('${profilePicture2}', 'hex')),
-  ('714418882', 'Admin', 'admin@gmail.com', '123', true, decode('${profilePicture3}', 'hex'));
+  ('824584468', 'José Coelho Carvalho', 'joaobunny@gmail.com', '123', false, decode('${profilePicture}', 'hex')),
+  ('519692670', 'Leonardo Kimura Marques', 'leokim@hotmail.com', '123', false, decode('${profilePicture}', 'hex')),
+  ('714418882', 'Admin', 'admin@gmail.com', '123', true, decode('${profilePicture}', 'hex'));
   `);
 }
