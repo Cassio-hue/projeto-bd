81669af003b94377619dda3bd3e15f932a9bfa14
Cassio-hue <cassiocvtb2003@gmail.com>
2023-07-13 00:37:09 -0300

    feat: funções do serviço de turma

--- backend/src/classes/classes.service.ts
+++ backend/src/classes/classes.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateClassDto } from './dto/create-class.dto';
 import { UpdateClassDto } from './dto/update-class.dto';
 import { CRUD } from '../utils';
@@ -13,8 +18,13 @@ export class ClassesService {
     this.CRUD = new CRUD(this.knex, this.table_name);
   }
 
-  create(createClassDto: CreateClassDto) {
-    return 'This action adds a new class';
+  async create(createClassDto: CreateClassDto) {
+    await this.CRUD.create(createClassDto).catch((err) => {
+      if (err.code == '23505')
+        throw new UnprocessableEntityException(err.detail);
+      else throw new BadRequestException('Algo deu errado ao criar turma');
+    });
+    return 'Turma adicionada com sucesso';
   }
 
   async findAll() {
@@ -35,12 +45,22 @@ export class ClassesService {
       });
   }
 
-  update(id: number, updateClassDto: UpdateClassDto) {
-    return `This action updates a #${id} class`;
+  async update(id: number, updateClassDto: UpdateClassDto) {
+    await this.checkClassId(id);
+    await this.CRUD.update(id, updateClassDto).catch((err) => {
+      if (err.code == '23505')
+        throw new UnprocessableEntityException(err.detail);
+      else
+        throw new BadRequestException(
+          'Algo deu errado ao atualizar departamento',
+        );
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} class`;
+  async remove(id: number) {
+    await this.checkClassId(id);
+    await this.CRUD.delete(id).catch();
+    return 'Turma removida com sucesso';
   }
 
   async classCompleteInfo() {
@@ -49,5 +69,11 @@ export class ClassesService {
         throw Error('Erro ao listar os dados da VIEW vw_class_details');
       })
       .then((res) => res);
+  }
+
+  async checkClassId(id: number) {
+    const classUnB = await this.findOne(id);
+    if (classUnB.length == 0)
+      throw new NotFoundException('Id fornecido não foi encontrado');
   }
 }
